3e84a87fb6912205a47486f22b871096b35559ee
Jonnes Nascimento <jonnesfn@ipt.br>
2023-10-18 16:17:53 -0300

    #thl-22 - Preventing double-tap zoom on iOS

--- server/src/test/java/br/ipt/thl/junit/AbstractTest.java
+++ server/src/test/java/br/ipt/thl/junit/AbstractTest.java
@@ -10,7 +10,6 @@ import org.mockito.verification.VerificationMode;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
