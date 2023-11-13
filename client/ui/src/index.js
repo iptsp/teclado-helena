5cafd8e644796d364f83bdca2e034cd6579fe8c1
Ale <alexandrefm@ipt.br>
2023-11-13 09:59:56 -0300

    Add Esc key

--- client/ui/src/index.js
+++ client/ui/src/index.js
@@ -35,17 +35,15 @@ const Event = {
 }
 
 const inputText = async (text, event) => {
-    if (text) {
-        const request = {
-            method: 'POST',
-            body: JSON.stringify({text, event}),
-            headers: {
-                'Content-Type': 'application/json'
-            }
+    const request = {
+        method: 'POST',
+        body: JSON.stringify({text, event}),
+        headers: {
+            'Content-Type': 'application/json'
         }
-        const response = await fetch(`${endpoint}/systems/keyboards/inputs`, request);
-        return await response.json();
     }
+    const response = await fetch(`${endpoint}/systems/keyboards/inputs`, request);
+    return await response.json();
 }
 
 const leftClick = async () => {
