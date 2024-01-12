8154818c08b519b34929d7c0166a93bc9a2c93a6
Pedro Generoso Vique Dantas <pdantas1@lenovo.com>
2024-01-12 14:16:18 -0300

    thl 57 - add GPL3 licensing

--- server/src/main/java/br/ipt/thl/executors/ExecutorsConfig.java
+++ server/src/main/java/br/ipt/thl/executors/ExecutorsConfig.java
@@ -1,3 +1,20 @@
+//Teclado Helena is a keyboard designed to better the experience mainly of users with cerebral palsy.
+//        Copyright (C) 2024  Instituto de Pesquisas Tecnológicas
+//This file is part of Teclado Helena.
+//
+//        Teclado Helena is free software: you can redistribute it and/or modify
+//        it under the terms of the GNU General Public License as published by
+//        the Free Software Foundation, either version 3 of the License, or
+//        (at your option) any later version.
+//
+//        Teclado Helena is distributed in the hope that it will be useful,
+//        but WITHOUT ANY WARRANTY; without even the implied warranty of
+//        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//        GNU General Public License for more details.
+//
+//        You should have received a copy of the GNU General Public License
+//        along with Teclado Helena. If not, see <https://www.gnu.org/licenses/>6.
+
 package br.ipt.thl.executors;
 
 
