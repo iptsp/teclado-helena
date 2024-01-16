4a6123050be52db6ee249a51106818c7ba68fdb9
Leonardo Akira Shimabukuro <leonardoashimabukuro@gmail.com>
2024-01-16 18:09:29 -0300

    add build script

---
+++ build.rs
@@ -0,0 +1,15 @@
+use npm_rs::*;
+
+fn main() {
+    println!("cargo:rerun-if-changed=client/");
+
+    let exit_status = NpmEnv::default()
+    .with_node_env(&NodeEnv::from_cargo_profile().unwrap_or_default())
+    .with_env("FOO", "bar")
+    .set_path("./client")
+    .init_env()
+    .install(None)
+    .run("build")
+    .exec().unwrap();
+    println!("exit status: {}", exit_status);
+}
