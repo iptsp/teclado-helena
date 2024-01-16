3e91226540d3d17989b27e307b681f4feb3a1900
Leonardo Akira Shimabukuro <leonardoashimabukuro@gmail.com>
2024-01-16 19:44:36 -0300

    ci test

--- build.rs
+++ build.rs
@@ -1,15 +1,14 @@
 use npm_rs::*;
 
 fn main() {
-    println!("cargo:rerun-if-changed=client/");
-
     let exit_status = NpmEnv::default()
-    .with_node_env(&NodeEnv::from_cargo_profile().unwrap_or_default())
-    .with_env("FOO", "bar")
-    .set_path("./client")
-    .init_env()
-    .install(None)
-    .run("build")
-    .exec().unwrap();
+        .with_node_env(&NodeEnv::from_cargo_profile().unwrap_or_default())
+        .with_env("FOO", "bar")
+        .set_path("./client")
+        .init_env()
+        .install(None)
+        .run("build")
+        .exec()
+        .unwrap();
     println!("exit status: {}", exit_status);
 }
