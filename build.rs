522b809d52778ba9de7dfdd8bc88b6326216c5c7
Leonardo Akira Shimabukuro <leonardoashimabukuro@gmail.com>
2024-01-19 16:04:12 -0300

    request highestAvailable execution level

--- build.rs
+++ build.rs
@@ -3,6 +3,19 @@ fn main() {
         let mut res = winres::WindowsResource::new();
         res.set_icon("assets/favicon.ico")
             .set_language(0x0416)
+            .set_manifest(
+                r#"
+            <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+            <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+                <security>
+                    <requestedPrivileges>
+                        <requestedExecutionLevel level="highestAvailable" uiAccess="false" />
+                    </requestedPrivileges>
+                </security>
+            </trustInfo>
+            </assembly>
+            "#,
+            )
             .compile()
             .unwrap();
     }
