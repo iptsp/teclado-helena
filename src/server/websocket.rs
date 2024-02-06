cef3b059e91cafb88aabdebc02643c05c75ce764
Leonardo Akira Shimabukuro <leonardoashimabukuro@gmail.com>
2024-02-06 15:07:42 -0300

    Update enigo to git version

--- src/server/websocket.rs
+++ src/server/websocket.rs
@@ -6,6 +6,7 @@ use axum::{
     extract::{ws::WebSocket, WebSocketUpgrade},
     response::Response,
 };
+use enigo::{Enigo, Settings};
 
 use crate::actions::*;
 
@@ -14,11 +15,13 @@ pub async fn handler(ws: WebSocketUpgrade) -> Response {
 }
 
 pub async fn handle_socket(mut socket: WebSocket) {
+    let mut enigo = Enigo::new(&Settings::default()).unwrap();
+
     while let Some(msg) = socket.recv().await {
         if let Ok(msg) = msg {
             let msgtext = msg.into_text().unwrap_or_default();
             if let Ok(action) = deserialize_action(&msgtext) {
-                process_action(action);
+                process_action(action, &mut enigo);
             } else {
                 println!("Invalid action");
             }
